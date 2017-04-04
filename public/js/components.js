6985813f93d444ee93ede5724f158bc304301180
msatghar <msatghar@stevens.edu>
2017-04-04 18:51:08 -0400

    Added Loan Options and Web Scraping
    Added input for loan option, and displayed the ROI for each option
    after scraping the loan website. Also displayed additional loan
    information above the chart.

--- public/js/components.js
+++ public/js/components.js
@@ -1,40 +1,80 @@
 
 var app = angular.module("myWidget", ["ngRoute", "ngResource", "chart.js"]);
 
-app.controller('widgetCtrl', function ($scope) {
+app.controller('widgetCtrl', function ($scope, $http) {
 
-    $scope.makeChart = function () {
-        $scope.labels = [];
-        let year = 2016;
-        for (let i = 0; i < $scope.period; i = i + 12) {
-            year++;
-            $scope.labels.push(year + "");
+    $scope.loanList = [];
+    $scope.loanTypes = [{ id: 1, name: "Direct Subsidized Loan" }, { id: 2, name: "Direct Unsubsidized Loan" }, { id: 3, name: "Private Bank Loan" }, { id: 4, name: "University Loan" }];
+
+    let subROI, unsubROI, privateROI = 4.2, uniROI = 4.0;
+
+    $scope.updateForm = function (item) {
+        $scope.loanList.push(item.name);
+        var roiDiv = angular.element(document.querySelector('#roiTxt'));
+        if (item.id == 1) {
+            $http.get("/roi/1").success(function (data) {
+                $scope.roi = "The current rate of interest for this type of loan is: " + data.roi + "%";
+                subROI = data.roi;
+                roiDiv.removeClass("hidden");
+            });
         }
-
-        let p = $scope.amount;
-        let r = $scope.interest / 12;
-        let n = $scope.period;
-        
-        $scope.emi = (p * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
-        let arr = [];
-
-        if ($scope.period < 12) {
-            arr.push($scope.emi * $scope.period);
+        else if (item.id == 2) {
+            $http.get("/roi/2").success(function (data) {
+                $scope.roi = "The current rate of interest for this type of loan is: " + data.roi + "%";
+                unsubROI = data.roi;
+                roiDiv.removeClass("hidden");
+            });
         }
-        else {
-            let p = $scope.period % 12;
-            for (let i = $scope.period - p; i > 0; i = i - 12) {
-                arr.push($scope.emi * 12);
-            }
-            if (p !== 0)
-                arr.push($scope.emi * p);
+        else if (item.id == 3) {
+            $scope.roi = "The current rate of interest for this type of loan is: " + privateROI + "%";
+            roiDiv.removeClass("hidden");
         }
-
-        $scope.data = [
-            arr
-        ];
+        else if (item.id == 4) {
+            $scope.roi = "The current rate of interest for this type of loan is: " + uniROI + "%";
+            roiDiv.removeClass("hidden");
+        }
     };
 
+    $scope.makeChart = function () {
 
+        if ($scope.amount && $scope.interest && $scope.period && $scope.loanType) {
+            $scope.labels = [];
+            let year = 2016;
+            for (let i = 0; i < $scope.period; i = i + 12) {
+                year++;
+                $scope.labels.push(year + "");
+            }
+
+            let p = $scope.amount;
+            let r = $scope.interest / 12;
+            let n = $scope.period;
+            let emi = (p * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+            let total = emi * n;
+            let totalInterest = total - p;
+            let arr = [];
+    
+            $scope.emi = "Monthly installments required: " + emi.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+            $scope.total = "Total amount to be paid: " + total.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+            $scope.totalInterest = "Total interest to be paid: " + totalInterest.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+            var infoDiv = angular.element(document.querySelector('#infoDiv'));
+            infoDiv.removeClass("hidden");
+
+            if ($scope.period < 12) {
+                arr.push(emi * $scope.period);
+            }
+            else {
+                let p = $scope.period % 12;
+                for (let i = $scope.period - p; i > 0; i = i - 12) {
+                    arr.push(emi * 12);
+                }
+                if (p !== 0)
+                    arr.push(emi * p);
+            }
+
+            $scope.data = [
+                arr
+            ];
+        }
+    };
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,39 @@
-// const recipeRoutes = require("./recipes");
+const request = require('request');
+const cheerio = require('cheerio');
 
 const constructorMethod = (app) => {
     // app.use("/recipes", recipeRoutes);
     app.get("/", (req, res) => {
-        res.render("form", {});
+        res.render("form", { roiList: [{ id: 1, roi: 5 }, { id: 2, roi: 7 }] });
     });
-    
-    app.use("*", (req, res) => {
-        res.redirect("/");
-    })
-};
 
-module.exports = constructorMethod;
+    app.get("/roi/:id", (req, res) => {
+       let url = 'http://studentaid.ed.gov/sa/types/loans/interest-rates';
+
+        request(url, function (error, response, html) {
+            if (!error) {
+                let $ = cheerio.load(html);
+
+                let subROI, unsubROI;
+
+                $('tbody').each(function(i,elem){
+                    if(i == 0){
+                    subROI = this.children[2].children[4].children[1].children[0].data;
+                    unsubROI = this.children[3].children[4].children[1].children[0].data;
+                    }
+                });
+
+                if (req.params.id == "1")
+                res.json({ roi: subROI.substring(0,subROI.length -1) });
+                if (req.params.id == "2")
+                res.json({ roi: unsubROI.substring(0,unsubROI.length -1)  });
+
+            }});
+            });
+
+        app.use("*", (req, res) => {
+            res.redirect("/");
+        })
+    };
+
+    module.exports = constructorMethod;
